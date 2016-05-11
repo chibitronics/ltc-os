048a2af8ad679c822f83d0b5e092428365b3ca74
Sean Cross <xobs@kosagi.com>
2016-05-11 14:56:03 +0800

    svc_handler: redirect syscall and return
    Rather than processing the syscall in an interrupt context, rewrite the
    link register and return.  This allows us to service interrupts when
    in the process of servicing syscalls.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/svc_handler.s
+++ orchard/svc_handler.s
@@ -1,34 +1,42 @@
     /* SVC_Handler code adapted from ARM docs.
-     * Determine what mode we're in, and load the appropriate stack to r7.
-     * Then, load the svc number, which is stored on the stack.
+     * Determine what mode we're in, and load the appropriate stack to r0.
+     * Then, load the svc number, which is not stored anywhere.  Instead,
+     * we look at the return address (stored at -6 on the stack), and examine
+     * the final byte of the opcode.
      * Finally, use this as an index into SysCall_Table for dispatch.
+     * We then overwrite the return value, and return.
      */
     .global SVC_Handler
     .func SVC_Handler
 SVC_Handler:
-    mov r7, lr
-    mov r6, #4
-    tst r7, r6
+    mov r0, lr
+    mov r1, #4
+    tst r0, r1
     beq svc_handler_msp
     bne svc_handler_psp
 svc_handler_msp:
-    mrs r7, MSP
+    mrs r0, MSP
     b svc_handler_dispatch
 svc_handler_psp:
-    mrs r7, PSP
+    mrs r0, PSP
     b svc_handler_dispatch
+    /* r0 now contains the stack:
+    * [ r0, r1, r2, r3, r12, r14, the return address and xPSR ]
+    */
 svc_handler_dispatch:
-    mov     r6, #6*4
-    ldr     r7, [r7, r6]
-    mov     r6, #0
-    sub     r6, #2
-    ldrb    r7, [r7, r6]
+    mov     r1, #6*4      // Get return address
+    ldr     r2, [r0, r1]  // Save it in r1
+
+    /* Get the svc number, which is stored in ram as the last byte */
+    mov     r3, #0        // Index -2
+    sub     r3, #2        // Decrement ot get to -2
+    ldrb    r2, [r2, r3]  // Pull the last byte of the svc instruction into r2
     //svc_number = ((char *)svc_args[6])[-2];
-    ldr     r6, =SysCall_Table
-    lsl     r7, #2
-    add     r6, r7, r6
-    ldr     r6, [r6]
-    bx      r6
+    ldr     r3, =SysCall_Table  // Index from the start of SysCall_Table
+    lsl     r2, #2              // Multiply svc instruction by 4
+    ldr     r3, [r3, r2]        // Load the syscall address
+    str     r3, [r0, r1]        // Store the return value back on the stack
+    bx lr
     .endfunc
     .type SVC_Handler, %function
     .size SVC_Handler, .-SVC_Handler
