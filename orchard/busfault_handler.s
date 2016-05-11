62f715b8882c07e88f7493f0ca2fbc394ed8e2c6
Sean Cross <xobs@kosagi.com>
2016-05-11 15:02:30 +0800

    handlers: add various handlers that unwrap the stack
    Signed-off-by: Sean Cross <xobs@kosagi.com>

---
+++ orchard/busfault_handler.s
@@ -0,0 +1,31 @@
+     /* Determine what mode we're in, and load the appropriate stack to sp. */
+    .global BusFault_Handler
+    .func BusFault_Handler
+BusFault_Handler:
+    mov r7, lr
+    mov r6, #4
+    tst r7, r6
+    beq hardfault_handler_msp
+    bne hardfault_handler_psp
+
+hardfault_handler_msp:
+    mrs r7, MSP
+    mov r0, r7
+    mov r1, #1
+
+    mov sp, r0          // Restore the stack pointer
+
+    ldr r2, =BusFault_Handler_C
+    bx r2
+
+hardfault_handler_psp:
+    mrs r7, PSP
+    mov r0, r7          // Store the stack frame in register 0
+    mov r1, #0          // Set "is_isr" to 0
+    mov sp, r0          // Restore the stack pointer so "bt" works
+
+    ldr r2, =BusFault_Handler_C
+    bx r2
+    .endfunc
+    .type BusFault_Handler, %function
+    .size BusFault_Handler, .-BusFault_Handler
