62f768d9102af08c24dba32e0787e80cceacc2b7
Sean Cross <xobs@kosagi.com>
2016-05-13 18:55:44 +0800

    arduino-math: badly implement random()
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/arduino-math.cpp
+++ orchard/arduino-math.cpp
@@ -1,13 +1,24 @@
 #include "Arduino.h"
+#include "murmur3.h"
 
 long map(long value, long fromLow, long fromHigh, long toLow, long toHigh) {
   return 0;
 }
 
+static long long rand_val;
+#warning "Value exceeds max, min"
 long random(long min, long max) {
-  return 4;
+  int32_t val;
+
+  MurmurHash3_x86_32(&rand_val, sizeof(rand_val), 0, &val);
+
+  rand_val += val;
+
+  return val;
 }
 
 long randomSeed(unsigned long seed) {
+
+  rand_val = seed;
   return 0;
 }
