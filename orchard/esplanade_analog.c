e6c95fe5b044a4d7a2981a97745d2cb518ec34b3
Sean Cross <xobs@kosagi.com>
2016-05-18 12:19:28 +0800

    esplanade_analog: UNTESTED - slim down ISR
    As an experiment, severely trim down the ADC ISR to the bare minimum.
    This version removes the need to make the callback, and does not
    do much arithmetic in the actual handler.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/esplanade_analog.c
+++ orchard/esplanade_analog.c
@@ -7,44 +7,44 @@
 #include "esplanade_demod.h"
 
 bl_symbol_bss(static adcsample_t mic_sample[MIC_SAMPLE_DEPTH]);
+bl_symbol_bss(static uint32_t adc_current_index);
+bl_symbol_bss(static const volatile uint32_t *adc_ra);
 
 extern volatile uint8_t dataReadyFlag;
 extern volatile adcsample_t *bufloc;
 extern size_t buf_n;
 
 static void analog_fast_isr(void) {
-  ADCDriver *adcp = &ADCD1;
 
   /* Read the sample into the buffer */
-  adcp->samples[adcp->current_index++] = adcp->adc->RA;
+  mic_sample[adc_current_index++] = *adc_ra;//adcp->adc->RA;
 
-  /*  At the end of the buffer then we may be finished */
-  if (adcp->current_index == adcp->number_of_samples) {
+  /* If buffer 2 has filled, deliver samples and move to buffer 1 */
+  if (adc_current_index == MIC_SAMPLE_DEPTH) {
     /* Invokes the callback passing the 2nd half of the buffer.*/
-    size_t half = adcp->depth / 2;
-    size_t half_index = half * adcp->grpp->num_channels;
-    adcp->grpp->end_cb(adcp, adcp->samples + half_index, half);
-    adcp->current_index = 0;
+    size_t half = MIC_SAMPLE_DEPTH / 2;
+    size_t half_index = half * 1;
+
+    dataReadyFlag = 1;
+    bufloc = mic_sample + half_index;
+    buf_n = half;
+    adc_current_index = 0;
   }
 
-  if( adcp->current_index == (adcp->number_of_samples / 2) ) {
-    _adc_isr_half_code(&ADCD1);
+  /* If buffer 1 has filled up, deliver the samples and continue to buffer 2 */
+  else if (adc_current_index == (MIC_SAMPLE_DEPTH / 2)) {
+    dataReadyFlag = 1;
+    bufloc = mic_sample;
+    buf_n = MIC_SAMPLE_DEPTH / 2;
   }
 }
 
 static void adc_mic_end_cb(ADCDriver *adcp, adcsample_t *buffer, size_t n) {
   (void)adcp;
+  (void)buffer;
   (void)n;
 
-#if !DEMOD_DEBUG  // select this path for "normal" orchard operation
-  chSysLockFromISR();
-  chEvtBroadcastI(&adc_mic_event);
-  chSysUnlockFromISR();
-#else  // single-thread operation
-  dataReadyFlag = 1;
-  bufloc = buffer;
-  buf_n = n;
-#endif
+  /* This function is unused, as the callback isn't used in "fast" mode. */
 }
 
 
@@ -87,14 +87,12 @@ static const ADCConversionGroup adcgrpmic = {
 };
 
 void analogUpdateMic(void) {
-  //  adcAcquireBus(&ADCD1);
-  //  adcConvert(&ADCD1, &adcgrpmic, mic_sample, MIC_SAMPLE_DEPTH);
   adcStartConversion(&ADCD1, &adcgrpmic, mic_sample, MIC_SAMPLE_DEPTH);
-  //  adcReleaseBus(&ADCD1);
 }
 
 void analogStart(void) {
   adcFastISR = analog_fast_isr;
+  adc_ra = &ADCD1.adc->RA;
 }
 
 void analogStop(void) {
