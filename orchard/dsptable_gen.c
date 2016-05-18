a1986d98f0965025cb7d31e38008878d5e121d3c
Sean Cross <xobs@kosagi.com>
2016-05-18 16:41:03 +0800

    dsptable_gen: import stdint and stdbool
    Get rid of our own typedefs, and use those supplied by C.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/dsptable_gen.c
+++ orchard/dsptable_gen.c
@@ -3,19 +3,12 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 typedef unsigned char           byte;
 typedef unsigned short          word;
 typedef unsigned int            dword;
-typedef int                     bool;
-typedef signed char             int8_t;
-typedef unsigned char           uint8_t;
-typedef signed short int        int16_t;
-typedef unsigned short int      uint16_t;
-typedef signed int              int32_t;
-typedef unsigned int            uint32_t;
-typedef unsigned long long      uint64_t;
-typedef long long               int64_t;
 
 #include "esplanade_demod.h"
 
