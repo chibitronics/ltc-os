2e142e45c6509f2466f6fabd544cc3b49297183a
Sean Cross <xobs@kosagi.com>
2016-05-20 17:19:11 +0800

    arduino-chibios: add more threads functions
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/arduino-chibios.c
+++ orchard/arduino-chibios.c
@@ -21,6 +21,34 @@ thread_t *createThread(void *stack, size_t stack_size, int prio,
   return chThdCreateStatic(stack, stack_size, prio, thr_func, ptr);
 }
 
+msg_t suspendThread(thread_reference_t *trp) {
+  return chThdSuspendS(trp);
+}
+
+msg_t suspendThreadTimeout(thread_reference_t *trp, systime_t timeout) {
+  return chThdSuspendTimeoutS(trp, timeout);
+}
+
+void resumeThread(thread_reference_t *trp, msg_t msg) {
+  chThdResume(trp, msg);
+}
+
+void yieldThread(void) {
+  chThdYield();
+}
+
+void threadSleep(systime_t time) {
+  chThdSleep(time);
+}
+
+void threadSleepUntil(systime_t time) {
+  chThdSleepUntil(time);
+}
+
+msg_t waitThread(thread_t *tp) {
+  return chThdWait(tp);
+}
+
 void exitThread(msg_t msg) {
   chThdExit(msg);
 }
@@ -31,4 +59,8 @@ void setTimer(virtual_timer_t *vtp, systime_t delay, vtfunc_t vtfunc, void *par)
 
 void resetTimer(virtual_timer_t *vtp) {
   chVTReset(vtp);
+}
+
+uint32_t getSyscallABI(void) {
+  return 1;
 }
