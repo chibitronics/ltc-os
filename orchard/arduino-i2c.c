0cd67672b8763411ab1c195beef55876e4b83b3d
Sean Cross <xobs@kosagi.com>
2016-05-20 12:36:47 +0800

    arduino-i2c: correct spelling of i2cTransceive
    i- before e-.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/arduino-i2c.c
+++ orchard/arduino-i2c.c
@@ -6,19 +6,22 @@ static const I2CConfig i2c_config = {
   100000
 };
 
-static int i2c_started;
+static uint8_t i2c_started;
 
-msg_t i2cTranscieve(i2caddr_t addr,
+msg_t i2cTransceive(i2caddr_t addr,
                     const uint8_t *txbuf, size_t txbytes,
                     uint8_t *rxbuf, size_t rxbytes,
                     systime_t timeout) {
   msg_t ret;
-  
+
+  /* Only start I2C if we're using it.  If we're in this function,
+   * then obviously we're using it.
+   */
   if (!i2c_started) {
     i2cStart(i2cDriver, &i2c_config);
     i2c_started = 1;
   }
-  
+
   i2cAcquireBus(i2cDriver);
   ret = i2cMasterTransmitTimeout(i2cDriver, addr, txbuf, txbytes, rxbuf, rxbytes, timeout);
   i2cReleaseBus(i2cDriver);
@@ -26,6 +29,6 @@ msg_t i2cTranscieve(i2caddr_t addr,
 }
 
 int i2cSetupSlave(void) {
-  
+
   return -1;
 }
