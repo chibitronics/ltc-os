78926058a15c4a07fe578615a43e26c725531151
Sean Cross <xobs@kosagi.com>
2016-05-23 13:05:06 +0800

    arduino-flash: lock and unlock the system as necessary
    Since the app can no longer lock and unlock the system, do the locking
    as part of the syscall.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/arduino-flash.c
+++ orchard/arduino-flash.c
@@ -3,9 +3,19 @@
 
 
 int8_t flashErase(uint32_t sectorOffset, uint16_t sectorCount) {
-  return flashEraseSectors(sectorOffset,  sectorCount);
+  uint8_t ret;
+
+  osalSysLock();
+  ret = flashEraseSectors(sectorOffset,  sectorCount);
+  osalSysUnlock();
+  return ret;
 }
 
 int8_t flashWrite(uint8_t *src, uint8_t *dst, uint32_t count) {
-  return flashProgram(src, dst, count);
+  uint8_t ret;
+
+  osalSysLock();
+  ret = flashProgram(src, dst, count);
+  osalSysUnlock();
+  return ret;
 }
