34264f6ea793fe58a8d3f2438fdc1a8f93ee7f9b
Sean Cross <xobs@kosagi.com>
2016-05-23 13:03:58 +0800

    arduino-chibios: make timers post to a callback queue
    We now post timers to a callback queue, in order to allow them
    to operate in a non-ISR context.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/arduino-chibios.c
+++ orchard/arduino-chibios.c
@@ -1,4 +1,6 @@
 #include "hal.h"
+#include "esplanade_app.h"
+#include "orchard.h"
 
 void lockSystem(void) {
   osalSysLock();
@@ -53,8 +55,78 @@ void exitThread(msg_t msg) {
   chThdExit(msg);
 }
 
-void setTimer(virtual_timer_t *vtp, systime_t delay, vtfunc_t vtfunc, void *par) {
-  chVTSet(vtp, delay, vtfunc, par);
+
+struct vt_callback {
+  vtfunc_t func;
+  void *param;
+};
+
+static struct vt_callback vtcallbacks[4];
+static struct vt_callback scheduled_callbacks[4];
+static uint8_t num_scheduled_callbacks = 0;
+
+int runCallbacks(void) {
+
+  unsigned int i;
+
+  if (!num_scheduled_callbacks)
+    return 0;
+
+  for (i = 0; i < ARRAY_SIZE(scheduled_callbacks); i++) {
+    if (scheduled_callbacks[i].func) {
+      scheduled_callbacks[i].func(scheduled_callbacks[i].param);
+      scheduled_callbacks[i].func = NULL;
+      num_scheduled_callbacks--;
+    }
+  }
+
+  return 0;
+}
+
+
+static void post_timer_callback_to_arduino_thread(void *par) {
+
+  unsigned int i;
+  struct vt_callback *vtcallback = par;
+  void *param = vtcallback->param;
+  vtfunc_t func = vtcallback->func;
+
+  /* Nullify these parameters so they can be requeued. */
+  vtcallback->param = NULL;
+  vtcallback->func  = NULL;
+
+  for (i = 0; i < ARRAY_SIZE(scheduled_callbacks); i++) {
+    if (!scheduled_callbacks[i].func) {
+      scheduled_callbacks[i].func = func;
+      scheduled_callbacks[i].param = param;
+      num_scheduled_callbacks++;
+      return;
+    }
+  }
+}
+
+int setTimer(virtual_timer_t *vtp,
+             systime_t delay,
+             vtfunc_t vtfunc,
+             void *par) {
+  unsigned int i;
+  int found = 0;
+  for (i = 0; i < ARRAY_SIZE(vtcallbacks); i++) {
+    if (!vtcallbacks[i].func) {
+      vtcallbacks[i].func = vtfunc;
+      vtcallbacks[i].param = par;
+      found = 1;
+      break;
+    }
+  }
+
+  /* We couldn't find a free slot for our callback array.  Sad times. */
+  if (!found)
+    return -1;
+
+  chVTSet(vtp, delay, post_timer_callback_to_arduino_thread, &vtcallbacks[i]);
+
+  return 0;
 }
 
 void resetTimer(virtual_timer_t *vtp) {
