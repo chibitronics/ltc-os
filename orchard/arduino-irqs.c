93eb9346e9d978abfef0b91f9b73c51c64787e94
Sean Cross <xobs@kosagi.com>
2016-05-25 16:52:36 +0800

    arduino-irqs: add attach/detachFastInterrupt
    This function will catch interrupts before the ChibiOS code runs.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- orchard/arduino-irqs.c
+++ orchard/arduino-irqs.c
@@ -8,3 +8,11 @@ void attachInterrupt(int irq, void (*func)(void), enum irq_mode mode) {
 void detachInterrupt(int irq) {
   return;
 }
+
+void attachFastInterrupt(int irq, void (*func)(void)) {
+  return;
+}
+
+void detachFastInterrupt(int irq) {
+  return;
+}
