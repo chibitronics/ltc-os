566a8fb02ee58432e2fcbb9b6b6c1f4931430ed2
Sean Cross <sean@xobs.io>
2017-01-19 18:12:51 +0800

    orchard: bump version to 1.8.3
    Signed-off-by: Sean Cross <sean@xobs.io>

--- orchard/orchard.h
+++ orchard/orchard.h
@@ -5,8 +5,8 @@ extern struct evt_table orchard_events;
 
 void halt(void);
 
-/* Version 1.8.2 */
-#define LTC_OS_VERSION 0x00010802
+/* Version 1.8.3 */
+#define LTC_OS_VERSION 0x00010803
 
 /* LTC board, PVT, 1, no patches */
 #define LTC_HW_VERSION 0x00030100
