9d67b3ac618e8fa515b3aa839509f53f3703fa17
Sean Cross <sean@xobs.io>
2017-02-14 16:51:03 +0800

    orchard: remove unused values from header file
    There is some extra cruft hanging out in orchard.h from when it used to
    be the Orchard platform.  Remove that.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- orchard/orchard.h
+++ orchard/orchard.h
@@ -1,25 +1,17 @@
 #ifndef __ORCHARD_H__
 #define __ORCHARD_H__
 
-extern struct evt_table orchard_events;
-
-void halt(void);
-
 /* Version 1.8.6 */
 #define LTC_OS_VERSION 0x00010806
 
 /* LTC board, PVT, 1, no patches */
 #define LTC_HW_VERSION 0x00030100
 
-#define ORCHARD_OS_VERSION_MAJOR      1
-#define ORCHARD_OS_VERSION_MINOR      0
-
 #define serialDriver                  (&SD1)
 #define stream_driver                 ((BaseSequentialStream *)serialDriver)
 extern void *stream;
 
 #define i2cDriver                     (&I2CD2)
-#define accelAddr                     0x1c
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(*x))
