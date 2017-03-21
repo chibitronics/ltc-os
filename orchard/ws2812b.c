ae6f18e80a4a0a9fa123f01c8f6c42a3bf3c3152
Sean Cross <sean@xobs.io>
2017-03-21 20:19:29 +0800

    ws2812b: canonicalize pins and use standard functions
    Use the canonical pin name to decide which pin to operate on.
    
    Also, check for sudo mode, and generally bounds-check what we get.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- orchard/ws2812b.c
+++ orchard/ws2812b.c
@@ -1,14 +1,33 @@
 #include <stdint.h>
 #include "hal.h"
 #include "Arduino.h"
+#include "arduino-io.h"
 
 extern void ledUpdate(uint32_t num_leds, void *pixels, uint32_t mask,
                       uint32_t set_addr, uint32_t clr_addr);
 void ledShow(uint32_t pin, void *pixels, uint32_t num_leds) {
 
+  ioportid_t port;
+  uint8_t pad;
+  uint32_t base;
+
+  pin = canonicalizePin(pin);
+
+  /* Don't let users access illegal pins.*/
+  if (!canUsePin(pin))
+    return;
+
+  if (pinToPort(pin, &port, &pad) < 0)
+    return;
+
+  if (port == IOPORT1)
+    base = FGPIOA_BASE;
+  else if (port == IOPORT1)
+    base = FGPIOB_BASE;
+
   chSysLock();
-  ledUpdate(num_leds, pixels, digitalPinToBitMask(pin),
-      digitalPinToPort(pin) + GPIO_PSOR,
-      digitalPinToPort(pin) + GPIO_PCOR);
+  ledUpdate(num_leds, pixels, 1 << pad,
+      base + GPIO_PSOR,
+      base + GPIO_PCOR);
   chSysUnlock();
 }
