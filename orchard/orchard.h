24075681c255ed9e627f510fd69d0b7e83549229
Sean Cross <sean@xobs.io>
2017-06-27 13:12:43 +0800

    ltc: tag 1.9.1
    This tags release 1.9.1, which is the first full production release.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- orchard/orchard.h
+++ orchard/orchard.h
@@ -1,11 +1,8 @@
 #ifndef __ORCHARD_H__
 #define __ORCHARD_H__
 
-/* Version 1.8.8 */
-#define LTC_OS_VERSION 0x00010808
-
-/* LTC board, PVT, 1, no patches */
-#define LTC_HW_PVT1C 0x00030100
+/* Version 1.9.1 */
+#define LTC_OS_VERSION 0x00010901
 
 #define serialDriver                  (&SD1)
 #define stream_driver                 ((BaseSequentialStream *)serialDriver)
